21ff7f5248e5675190618d46bde13898bf599598
malikyous <malikyousaf1404@gmail.com>
2026-07-08 01:49:23 -0700

    Fix app initialization in backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -77,7 +77,7 @@ def seed_data():
     db.session.add_all(products)
     db.session.commit()
 
-
+app = create_app()
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True, port=5000)
